b6573318222120eba52ded4f611638bc3d069e17
safan9 <patelsafan99@gmail.com>
2020-12-26 10:30:53 +0530

    Added meta tags

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Shop</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<title>Shop @yield('seo_title')</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+	crossorigin="anonymous">
 
 	@yield('header_files')
 
--- resources/views/shop/show.blade.php
+++ resources/views/shop/show.blade.php
@@ -1,12 +1,22 @@
 @extends('shop.base')
 
+
+@section('seo_title')
+	| {{$product->seo_title}}
+@endsection
+
+@section('header_files')
+	<meta name="description" content="{{$product->seo_description}}">
+@endsection
+
 @section('content')
 	<div class="row">
 	   <div class="col-md-6">
 	    	<img
 				src="{{ asset('storage/images/product') }}/{{$product->image}}"
 				alt="{{$product->image}}"
-				class="img-fluid img-thumbnail" width="100%" height="60%"
+				class="img-fluid img-thumbnail" 
+				width="100%" height="60%"
 				/>
 	   </div>
 	   <div class="col-md-6">
